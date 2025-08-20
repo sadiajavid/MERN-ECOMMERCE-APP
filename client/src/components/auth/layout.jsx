9232a8865d9b65af0f58fad041fb91f14f4daa8e
sadia javid <sadiajavid31@gmail.com>
2025-08-20 16:00:10 +0500

    chnges added

--- client/src/components/auth/layout.jsx
+++ client/src/components/auth/layout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 
 const Authlayout = () => {
   return (
-    <div className='flex min-h-screen  w-full'>
+    <div className='flex min-h-screen    w-full'>
       <div className='lg:flex items-center justify-center bg-black w-1/2 px-12'>
         <div className='max-w-md space-y-6 text-center text-white'>
           <h1 className='text-4xl font-extrabold tracking-tight'>
